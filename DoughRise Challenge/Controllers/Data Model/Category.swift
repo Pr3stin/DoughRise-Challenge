f7108cad34343daf30db041a19cdf5188441876d
Prestin <lauprestin@gmail.com>
2023-05-13 13:46:50 -0400

    New Commit: added tableView data model

---
+++ DoughRise Challenge/Controllers/Data Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  DoughRise Challenge
+//
+//  Created by Prestin Lau on 5/13/23.
+//
+
+import Foundation
+
+class Category {
+    var title: String = ""
+    var spent: String = ""
+    var left: String = ""
+}
--- DoughRise Challenge/Controllers/HomeViewController.swift
+++ DoughRise Challenge/Controllers/HomeViewController.swift
@@ -7,25 +7,46 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var MonthlyBudgetView: UIView!
-    
     @IBOutlet weak var CategoryTableView: UITableView!
     @IBOutlet weak var BudgetBreakdownView: UIView!
+    @IBOutlet weak var AddButton: UIButton!
     
+    var category = [Category]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // UI
+        
         MonthlyBudgetView?.layer.cornerRadius = 25
         BudgetBreakdownView?.layer.cornerRadius = 25
         BudgetBreakdownView?.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         CategoryTableView?.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
         CategoryTableView?.layer.cornerRadius = 25
-        
+        AddButton?.layer.cornerRadius = 0.5 * AddButton.bounds.size.width
         
     }
     
-
+    // Category TableView
+    
+    class CustomCell : UITableViewCell {
+        @IBOutlet weak var CellImageLabel: UILabel!
+        @IBOutlet weak var CellTitleLabel: UILabel!
+        @IBOutlet weak var CellSpentLabel: UILabel!
+        @IBOutlet weak var CellLeftLabel: UILabel!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
+        
+    }
+    
+    
+    
 }
